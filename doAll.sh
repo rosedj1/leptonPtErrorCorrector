fd8e02ef3874f17d7ab17741f00264bddc642d10
mhl0116 <mhl0116>
2016-09-23 05:12:25 -0400

    first commit from HZZ4L_Mass repository

---
+++ doAll.sh
@@ -0,0 +1,18 @@
+python getLambda2.py --ptLow 10 --ptHigh 40 --etaLow 0 --etaHigh 0.7 --lambda1_pre 1.119 --fs e &
+python getLambda2.py --ptLow 10 --ptHigh 40 --etaLow 0.7 --etaHigh 1.5 --lambda1_pre 1.119 --fs e &
+python getLambda2.py --ptLow 40 --ptHigh 50 --etaLow 0 --etaHigh 0.7 --lambda1_pre 1.119 --fs e &
+python getLambda2.py --ptLow 40 --ptHigh 50 --etaLow 0.7 --etaHigh 1.5 --lambda1_pre 1.119 --fs e &
+python getLambda2.py --ptLow 50 --ptHigh 100 --etaLow 0 --etaHigh 0.7 --lambda1_pre 1.119 --fs e &
+python getLambda2.py --ptLow 50 --ptHigh 100 --etaLow 0.7 --etaHigh 1.5 --lambda1_pre 1.119 --fs e &
+python getLambda2.py --ptLow 10 --ptHigh 100 --etaLow 1.5 --etaHigh 1.6 --lambda1_pre 1.119 --fs e &
+python getLambda2.py --ptLow 10 --ptHigh 100 --etaLow 1.6 --etaHigh 2.5 --lambda1_pre 1.119 --fs e &
+python getLambda2.py --ptLow 10 --ptHigh 40 --etaLow 0 --etaHigh 0.9 --lambda1_pre 1.23 --fs mu &
+python getLambda2.py --ptLow 10 --ptHigh 40 --etaLow 0.9 --etaHigh 1.2 --lambda1_pre 1.23 --fs mu &
+python getLambda2.py --ptLow 10 --ptHigh 40 --etaLow 1.2 --etaHigh 2.4 --lambda1_pre 1.23 --fs mu &
+python getLambda2.py --ptLow 40 --ptHigh 50 --etaLow 0 --etaHigh 0.9 --lambda1_pre 1.23 --fs mu &
+python getLambda2.py --ptLow 40 --ptHigh 50 --etaLow 0.9 --etaHigh 1.2 --lambda1_pre 1.23 --fs mu &
+python getLambda2.py --ptLow 40 --ptHigh 50 --etaLow 1.2 --etaHigh 2.4 --lambda1_pre 1.23 --fs mu &
+python getLambda2.py --ptLow 50 --ptHigh 100 --etaLow 0 --etaHigh 0.9 --lambda1_pre 1.23 --fs mu &
+python getLambda2.py --ptLow 50 --ptHigh 100 --etaLow 0.9 --etaHigh 1.2 --lambda1_pre 1.23 --fs mu &
+python getLambda2.py --ptLow 50 --ptHigh 100 --etaLow 1.2 --etaHigh 2.4 --lambda1_pre 1.23 --fs mu &
+
---
+++ doLambda1.sh
@@ -0,0 +1,7 @@
+#data
+time python getLambda1.py --ptLow 40 --ptHigh 50 --etaLow 0 --etaHigh 0.7 --fs e  --isData &
+time python getLambda1.py --ptLow 40 --ptHigh 50 --etaLow 0 --etaHigh 0.9 --fs mu --isData &
+#dy mc
+#time python getLambda1.py --ptLow 40 --ptHigh 50 --etaLow 0 --etaHigh 0.7 --fs e  &
+#time python getLambda1.py --ptLow 40 --ptHigh 50 --etaLow 0 --etaHigh 0.9 --fs mu &
+
