045c0ee6e451a4149df2bc203576830b5dd88a46
mhl0116 <mhl0116>
2016-12-07 09:27:29 -0500

    tmp

--- doCorrection/lambdas_singleCB/pT_50p0_100p0_eta_0p0_0p8_e_mc_v0.py
+++ doCorrection/lambdas_singleCB/pT_50p0_100p0_eta_0p0_0p8_e_mc_v0.py
@@ -1,1 +1,1 @@
-lambdas = {'lambda1': 1.17654405301, 'lambda2': 1.0, 'lambda': 1.1413238085002835}
+lambdas = {'lambda1': 0.975094787128, 'lambda2': 1.0, 'lambda': 1.3736668260365181}
--- doCorrection/lambdas_singleCB/pT_7p0_50p0_eta_0p0_0p8_e_mc.py
+++ doCorrection/lambdas_singleCB/pT_7p0_50p0_eta_0p0_0p8_e_mc.py
@@ -1,1 +1,1 @@
-lambdas = {'lambda1': 1.0, 'lambda2': 1.0, 'lambda': 1.095985516499224}
+lambdas = {'lambda1': 1.0, 'lambda2': 1.0, 'lambda': 0.9750947871280968}
