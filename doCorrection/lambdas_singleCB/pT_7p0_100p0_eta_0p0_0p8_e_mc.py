8dd318779c370e12f51a7702e424223fe8318dd9
mhl0116 <mhl0116>
2016-12-07 16:30:33 -0500

    https://www.evernote.com/Home.action#n=2e060f15-57f9-44ce-913a-55bd7a3f0778&ses=4&sh=2&sds=2&

--- doCorrection/lambdas_singleCB/pT_7p0_100p0_eta_0p0_0p8_e_mc.py
+++ doCorrection/lambdas_singleCB/pT_7p0_100p0_eta_0p0_0p8_e_mc.py
@@ -1,1 +1,1 @@
-lambdas = {'lambda1': 1.0, 'lambda2': 1.0, 'lambda': 1.2440319958676749}
+lambdas = {'lambda1': 1.0, 'lambda2': 1.0, 'lambda': 1.2695660343930626}
--- doCorrection/lambdas_singleCB/pT_7p0_100p0_eta_0p8_1p0_e_mc.py
+++ doCorrection/lambdas_singleCB/pT_7p0_100p0_eta_0p8_1p0_e_mc.py
@@ -1,1 +1,1 @@
-lambdas = {'lambda1': 1.0, 'lambda2': 1.0, 'lambda': 1.3202729765548316}
+lambdas = {'lambda1': 1.0, 'lambda2': 1.0, 'lambda': 1.3294249799290059}
